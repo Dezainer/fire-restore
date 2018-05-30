dd3c3beafac66c49654c28c9ea59a0c9d014b882
Dezainer <gameboymasterz@gmail.com>
2018-05-30 14:35:49 -0300

    Better than ever

--- src/actions/backup.js
+++ src/actions/backup.js
@@ -1,34 +1,38 @@
 module.exports = function(db, path) {
 	let initialPath = path ? db.doc(path) : db
-	return scavengeData(initialPath)
+	return scavengeDoc(initialPath)
 }
 
-function scavengeData(doc) {
+function scavengeDoc(doc) {
 	return getCollectionsFromDocument(doc).then(collections => {
 		let obj = {}
 
 		return collections.length == 0
 			? doc.get().then(result => result.data())
 			: Promise.all(collections.map(collection =>
-				getCollectionSubData(obj, doc, collection)
+				scavengeCollection(collection).then(subData => {
+					obj[collection.id] = subData
+				})
 			)).then(() => obj)
-	})
-}
-
-function getCollectionSubData(obj, doc, collection) {
-	return getCollectionData(collection).then(colData => {
-		obj[collection.id] = {}
-
-		return Promise.all(colData.map(doc =>
-			scavengeData(doc.ref).then(subData => {
-				obj[collection.id][doc.id] = Object.assign(doc.data(), subData)
-			})
-		))
 	})
 }
 
 function getCollectionsFromDocument(doc) {
 	return doc.getCollections().then(data => formatData(data))
+}
+
+function scavengeCollection(collection) {
+	return getCollectionData(collection).then(colData => {
+		let obj = {}
+
+		return colData.length == 0
+			? obj
+			: Promise.all(colData.map(doc =>
+				scavengeDoc(doc.ref).then(subData => {
+					obj[doc.id] = Object.assign(doc.data(), subData)
+				})
+			)).then(() => obj)
+	})
 }
 
 function getCollectionData(collection) {

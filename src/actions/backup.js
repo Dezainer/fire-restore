ba698c30eb7ba3ae769ce6174522f198fa400fe0
Dezainer <gameboymasterz@gmail.com>
2018-05-30 19:58:58 -0300

    Found some flaws on the doc part, fixed(had to do with the metadata as well)

--- src/actions/backup.js
+++ src/actions/backup.js
@@ -12,18 +12,40 @@ function scavengeDoc(doc) {
 	return getDocumentCollections(doc).then(collections => {
 		let obj = {}
 
-		return collections.length == 0
-			? doc.get().then(result => result.data())
-			: Promise.all(collections.map(collection =>
-				scavengeCollection(collection).then(subData => {
-					obj[collection.id] = subData
-				})
-			)).then(() => obj)
+		return getDocumentData(doc).then(data => {
+			if(data) obj.__FIELDS__ = data
+		}).then(() =>
+			getDocumentCollectionsData(collections).then((data) => {
+				if(data) obj.__COLLECTIONS__ = data
+			})
+		).then(() => obj)
 	})
 }
 
 function getDocumentCollections(doc) {
 	return doc.getCollections().then(data => Util.formatData(data))
+}
+
+function getDocumentData(doc) {
+	return new Promise((resolve, reject) => {
+		doc.get
+			? doc.get().then(result => resolve(result.data()))
+			: resolve()
+	})
+}
+
+function getDocumentCollectionsData(collections, callback) {
+	return new Promise((resolve, reject) => {
+		let obj = {}
+
+		collections.length == 0
+			? resolve()
+			: Promise.all(collections.map(collection =>
+				scavengeCollection(collection).then(subData => {
+					obj[collection.id] = subData
+				})
+			)).then(() => resolve(obj))
+	})
 }
 
 function scavengeCollection(collection) {
@@ -34,20 +56,10 @@ function scavengeCollection(collection) {
 			? obj
 			: Promise.all(colData.map(doc =>
 				scavengeDoc(doc.ref).then(subData => {
-					obj[doc.id] = arrangeDocData(doc.data(), subData)
+					obj[doc.id] = subData
 				})
 			)).then(() => obj)
 	})
-}
-
-function arrangeDocData(data, subData) {
-	let arranged = {}
-	arranged.__FIELDS__ = data
-
-	if(!Util.areObjectsEqual(data, subData))
-		arranged.__COLLECTIONS__ = subData
-
-	return arranged
 }
 
 function getCollectionData(collection) {
--- src/actions/restore.js
+++ src/actions/restore.js
@@ -1,3 +1,16 @@
+const Util = require('../util')
+var batch
+
 module.exports = function(db, path, obj) {
-	console.log('é isso né cara')
+	batch = db.batch()
+}
+
+function populateDoc(ref, obj) {
+	Util.mapObject(obj, collection => {
+		populateCollection(collection)
+	})
+}
+
+function populateCollection(ref, obj) {
+	
 }
